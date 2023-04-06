7540166f9bb40fe97a5f0411ad885a0b6cebf228
jackpothsu <sgx77798@gmail.com>
2023-04-06 20:41:46 +0800

    11

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(function () {
     $(".header").load("common.html .header>*");
     $(".footer").load("common.html .footer>*");
+
+    let width = $(window).width()
+  if(width < 415){ let htmlbrs = $(".ablr p")
+  for (let i = 0; i < htmlbrs.length; i++) {
+    let htmlbr = htmlbrs[i];
+    let htmlString = htmlbr.innerHTML;
+    let cleanedString = htmlString.replace(/<br\s*\/?>/gi, "");
+    htmlbr.innerHTML = cleanedString;
+  }}
+   
   });
+
+ 
